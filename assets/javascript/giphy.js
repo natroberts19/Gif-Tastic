7287ea013336eacadfc525d55b1cff17b5a9648e
Natalie Roberts <nr_roberts@yahoo.com>
2017-12-03 18:32:16 -0500

    more styling, added form input clear, static/animate function

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -2,7 +2,7 @@
 var topics = ["Justin Timberlake", "Melissa McCarthy", "Christopher Walken", "Alec Baldwin", "Steve Martin", "John Goodman", "Tina Fey"];
 console.log(topics);
 
-// Function for dumping the JSON content for each button into the div. Need to then isolate the rating and both static and animated urls.
+// Function for dumping the JSON content for each button. Need to isolate the rating and both static and animated urls.
 function displayGifInfo() {
     var giphyName = $(this).attr("data-item");
     console.log(giphyName);
@@ -27,25 +27,25 @@ function displayGifInfo() {
             var animatedURL = response.data[i].images.fixed_height.url;
              console.log("Animated Image: " + response.data[i].images.fixed_height.url);
             // Create an HTML image element to store the image and attach either a static or animated state
-            var image = $("<img>").attr("src", animatedURL);
+            var image = $("<img>").attr("src", staticURL);
             // Append the image to the gifDiv.
             gifDiv.append(image);
             // Create a variable to store the rating data.
             var gifRating = response.data[i].rating;
             // Create an HTML paragraph element to store the rating.
-            var para = $("<p>").text("Rating: " + gifRating);
+            var paragraph = $("<p>").text("Rating: " + gifRating);
             console.log(gifRating);
             // Display the rating
-            gifDiv.append(para);
-            // Put the gifs above each other
-            $("#giphy-dump").prepend(gifDiv);
+            gifDiv.append(paragraph);
+            // Put the gifs below each other
+            $("#giphy-dump").append(gifDiv);
         }
-        // This function will allow the user to click on a gif and make it animate or be still. Apply to the .gif class which has already been added to the gifDiv.
+        // This function will make each gif clickable. When clicked the gif will become animated. Click again to pause. Apply to the .gif class which has already been added to the gifDiv.
         $(".gif").on("click", function () {
            
             // Check if the variable state is equal to 'still'.
             var state = $(this).attr("data-state");
-            console.log(this);
+            console.log(state);
 
             // Then create if/else to toggle between static and animated.
             if (state === "still") {
@@ -69,6 +69,7 @@ function renderButtons() {
         a.addClass("gif");
         a.addClass("button-style");
         a.attr("data-item", topics[j]);
+        a.attr("data-state", topics[j]);
         a.text(topics[j]);
         $("#buttons-dump").append(a);
         console.log(a);
@@ -76,20 +77,22 @@ function renderButtons() {
 }
 
 // This function grabs the user input using a local variable of "giphy".
-// finalizes their giphy choice when the Search button is clicked. 
+// Finalizes the giphy topic choice when the Add button is clicked. 
 $("#find-giphy").on("click", function (event) {
     // event.preventDefault() prevents the form from trying to submit itself.
     // User can hit enter instead of clicking the button if they want.
     event.preventDefault();
 
-    // This line will grab the text from the input box
+    // This line will grab the text from the input box in the form.
     var giphy = $("#giphy-input").val().trim();
-    // The giphy from the textbox is then added to our array
+    // The giphy input category from the textbox is then added to the topics array.
     topics.push(giphy);
     console.log(giphy);
-
-    // calling renderButtons which handles the processing of our giphy array
+    
+    // Call the renderButtons function which handles the processing of our giphy array
     renderButtons();
+    // Clear the input text from the Giphy form after clicking the Add button.
+    $("#giphy-input").val("");
 });
 
 // links back to the generic function to capture Gif Info. Provides an event listener to all elements with class of .gif
